8a08afe7a18deb8db996ceddb4e8d59b5508a004
AmirolAhmad <dev.amirol@gmail.com>
2018-07-27 13:37:34 +0800

    As a user, I need an API to retrieve the friends list for an email address

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,5 +1,6 @@
 class FriendshipsController < ApplicationController
   def create
+    # do code if email already exists in db
     @friends = params[:friends]
 
     @friends.each do |key,value|
@@ -11,5 +12,11 @@ class FriendshipsController < ApplicationController
         render json: {success: true}
       end
     end
+  end
+
+  def list
+    @user = User.find_by_email params[:email]
+    @friendship = Friendship.where user: @user
+    render json: {success: true, friends: @friendship.map {|f| f.friend.email}, count: @friendship.count}
   end
 end
