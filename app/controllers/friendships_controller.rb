472b521b3042574d05c635fbfc5f5a22dcda15b1
AmirolAhmad <dev.amirol@gmail.com>
2018-07-27 14:39:41 +0800

    insert more logic to handle creating relationship

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,12 +1,27 @@
 class FriendshipsController < ApplicationController
   def create
-    # do code if email already exists in db
     @friends = params[:friends]
+    key = User.find_by_email @friends[0]
+    val = User.find_by_email @friends[1]
 
-    @friends.each do |key,value|
-      if @friends[0] == key
-        @user = User.create email: key
-        @friend = User.create email: value
+    if Friendship.where(user: key, friend: val).exists?
+      render json: {message: "Relationship already establish"}
+    else
+      if User.where(email: @friends[0]).exists? && !User.where(email: @friends[1]).exists?
+        @user = key
+        @friend = User.create email: @friends[1]
+        @user.friendships.create friend_id: @friend.id
+        render json: {success: true}
+
+      elsif !User.where(email: @friends[0]).exists? && User.where(email: @friends[1]).exists?
+        @user = User.create email: @friends[0]
+        @friend = User.find_by_email(@friends[1])
+        @user.friendships.create friend_id: @friend.id
+        render json: {success: true}
+        
+      else
+        @user = User.create email: @friends[0]
+        @friend = User.create email: @friends[1]
         @user.friendships.create friend_id: @friend.id
 
         render json: {success: true}
