6217004689800d787fa7f868ae1af13deea867c4
AmirolAhmad <dev.amirol@gmail.com>
2018-07-27 15:20:17 +0800

    I need an API to retrieve the common friends list between two email addresses

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,24 +1,32 @@
 class FriendshipsController < ApplicationController
-  def create
-    @friends = params[:friends]
-    key = User.find_by_email @friends[0]
-    val = User.find_by_email @friends[1]
+  before_action :set_friend, only: [:create]
 
-    if Friendship.where(user: key, friend: val).exists?
+  def create
+    if Friendship.where(user: @key, friend: @val).exists?
       render json: {message: "Relationship already establish"}
     else
       if User.where(email: @friends[0]).exists? && !User.where(email: @friends[1]).exists?
-        @user = key
+        @user = @key
         @friend = User.create email: @friends[1]
         @user.friendships.create friend_id: @friend.id
         render json: {success: true}
 
       elsif !User.where(email: @friends[0]).exists? && User.where(email: @friends[1]).exists?
         @user = User.create email: @friends[0]
-        @friend = User.find_by_email(@friends[1])
+        @friend = @val
         @user.friendships.create friend_id: @friend.id
         render json: {success: true}
-        
+
+      elsif User.where(email: @friends[0]).exists? && User.where(email: @friends[1]).exists?
+        if Friendship.where(user: @key, friend: @val).exists?
+          render json: {message: "Relationship already establish"}
+        else
+          @user = @key
+          @friend = @val
+          @user.friendships.create friend_id: @friend.id
+          render json: {success: true}
+        end
+
       else
         @user = User.create email: @friends[0]
         @friend = User.create email: @friends[1]
@@ -34,4 +42,35 @@ class FriendshipsController < ApplicationController
     @friendship = Friendship.where user: @user
     render json: {success: true, friends: @friendship.map {|f| f.friend.email}, count: @friendship.count}
   end
+
+  def common
+    @params = params[:friends]
+    a = JSON.parse(@params)
+    @key = User.find_by_email a[0]
+    @val = User.find_by_email a[1]
+
+    @friendship_key = Friendship.where user: @key
+    @friendship_val = Friendship.where user: @val
+
+    @lists_id = []
+    @friendship_key.each do |f|
+      @friendship_val.each do |g|
+        if f.friend_id == g.friend_id
+          @lists_id << g.friend_id
+        end
+      end
+    end
+
+    @user = User.find @lists_id
+
+    render json: {success: true, friends: @user.map {|f| f.email}, count: @lists_id.count}
+  end
+
+  private
+
+    def set_friend
+      @friends = params[:friends]
+      @key = User.find_by_email @friends[0]
+      @val = User.find_by_email @friends[1]
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post :friendships, to: 'friendships#create'
   get :friendlist, to: 'friendships#list'
+  get :common, to: 'friendships#common'
 end
